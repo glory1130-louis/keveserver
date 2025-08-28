e1dac711d5e7b86b5bd398ab63be3760213d907e
glory1130-louis <sr.semanager1130@gmail.com>
2025-08-28 07:09:22 -0500

    update config file

---
+++ config/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "123456",
+    database: "first",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
